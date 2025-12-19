74c16d6ab3239e5ed4991b776e2a13ca99bccaec
shailesh-00git <sy468698@gmail.com>
2025-12-19 18:44:31 +0545

    Adding and working with shadcn ui library

---
+++ 04-shadcn-ui/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button.tsx";
+function App() {
+  return (
+    <>
+      <Button variant={"default"}>click me</Button>
+      <Button variant={"default"}>click me</Button>
+      <Button variant={"default"}>click me</Button>
+    </>
+  );
+}
+
+export default App;
