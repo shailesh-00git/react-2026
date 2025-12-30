45dc746d6a1b7eb59f802f814065df040893fbc3
shailesh-00git <sy468698@gmail.com>
2025-12-30 22:27:43 +0545

    Basic props completed

--- 09-advance-prop-and-comp/src/App.jsx
+++ 09-advance-prop-and-comp/src/App.jsx
@@ -5,8 +5,6 @@ import RefProps from "./components/RefProps.jsx";
 import ThemeToggler from "./components/ThemeToggler.jsx";
 
 function Navigation() {
-  const isDark = true;
-
   const sections = [
     { id: "basic", label: "Basic Props", icon: "📦" },
     { id: "ref", label: "Ref Props", icon: "®️" },
@@ -14,20 +12,20 @@ function Navigation() {
     { id: "complex", label: "Complex Props", icon: "😥" },
     { id: "theme", label: "Theme Props", icon: "🌑" },
   ];
+
   return (
-    <nav className="sticky z-50 top-0 shadow-md">
-      <div className="flex justify-center items-center h-20 ">
-        <div>
-          {sections.map((section) => (
-            <button
-              className={`px-4 py-2 rounded-lg font-medium bg-blue-600 text-white mr-2 mt-2 hover:bg-blue-800 tarnsition-all duration-250`}
-              key={section.id}
-            >
-              <span className="mr-2">{section.icon}</span>
-              {section.label}
-            </button>
-          ))}
-        </div>
+    <nav className="sticky top-0 z-50 bg-gray-800 shadow-md">
+      <div className="flex justify-center items-center h-20">
+        {sections.map((section) => (
+          <a
+            key={section.id}
+            href={`#${section.id}`}
+            className="px-4 py-2 rounded-lg font-medium bg-blue-600 text-white mr-2 hover:bg-blue-800 transition-all"
+          >
+            <span className="mr-2">{section.icon}</span>
+            {section.label}
+          </a>
+        ))}
       </div>
     </nav>
   );
@@ -37,7 +35,7 @@ function AppContent() {
   return (
     <div className={`min-h-screen bg-gray-800`}>
       <Navigation />
-      <div className={`container mx-auto px-4 py-8`}>
+      <main className={`container mx-auto px-4 py-8`}>
         <header>
           <h1 className="text-center text-white text-5xl font-bold mb-6">
             React Props Explained
@@ -45,17 +43,34 @@ function AppContent() {
           <p className="text-center text-lg  text-white mb-2">
             A complete quide to understand and leran the react props
           </p>
-          <p className="text-center text-white">
+          <p className="text-center text-white mb-5">
             Build with: bun + react + vite + tailwindcss
           </p>
         </header>
 
         <div className="space-y-8">
-          <div id="baisc" className="scroll-mt-200">
+          <div id="basic" className="scroll-mt-200">
             <BasicProps />
           </div>
+          <div id="chidern" className="scroll-mt-200">
+            <ChildernProps />
+          </div>
+          <div id="complex" className="scroll-mt-200">
+            <ComplexProps />
+          </div>
+          <div id="ref" className="scroll-mt-200">
+            <RefProps />
+          </div>
+          <div id="theme" className="scroll-mt-200">
+            <ThemeToggler />
+          </div>
         </div>
-      </div>
+
+        {/* footer */}
+        <div className={` grid place-content-center text-white`}>
+          <p>Made with react, bun and love❤️</p>
+        </div>
+      </main>
     </div>
   );
 }
--- 09-advance-prop-and-comp/src/components/BasicProps.jsx
+++ 09-advance-prop-and-comp/src/components/BasicProps.jsx
@@ -1,7 +1,123 @@
 import React from "react";
 
+function Button({ text, color, size, onClick, disabled }) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`px-6 py-2 rounded-lg transition-all duration-300
+       ${size === "small" ? "text-sm px-3 py-1" : ""}
+       ${size === "medium" ? "text-lg px-6 py-2" : ""}
+        ${size === "large" ? "text-2xl px-8 py-3" : ""}
+        ${
+          color == "primary" ? "bg-blue-500 hover:bg-blue-600 text-white" : ""
+        }        
+        ${
+          color == "secondary" ? "bg-gray-500 hover:bg-gray-600 text-white" : ""
+        }        
+        ${
+          color == "danger" ? "bg-red-500 hover:bg-red-600 text-white" : ""
+        }        
+        ${
+          color == "success" ? "bg-green-500 hover:bg-green-600 text-white" : ""
+        } 
+        ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"}       
+        `}
+    >
+      {text}
+    </button>
+  );
+}
 function BasicProps() {
-  return <div className="text-white">BasicProps</div>;
+  const [clickCount, setClickCount] = React.useState(0);
+  return (
+    <section className="p-8 bg-white rounded-xl shadow-2xl">
+      <h3 className={` text-3xl font-bold text-gray-700`}>Basic Props</h3>
+      <p className={`mt-2`}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure dolorum
+        fugiat architecto mollitia quis omnis odio consequuntur placeat debitis
+        commodi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
+        eligendi iusto nisi temporibus mollitia possimus voluptas facilis esse
+        aperiam delectus!
+      </p>
+      <div className="space-y-4 mb-5 mt-5">
+        <h3 className={` text-2xl font-medium mb-2 text-gray-700`}>
+          Different colors
+        </h3>
+        <div className="flex flex-wrap gap-3">
+          <Button
+            text="Primary Button"
+            color="primary"
+            onClick={() => setClickCount(clickCount + 1)}
+          />
+          <Button
+            text="Secondary Button"
+            color="secondary"
+            onClick={() => setClickCount(clickCount + 1)}
+          />
+          <Button
+            text="Danger Button"
+            color="danger"
+            onClick={() => setClickCount(clickCount + 1)}
+          />
+          <Button
+            text="Success Button"
+            color="success"
+            onClick={() => setClickCount(clickCount + 1)}
+          />
+        </div>
+      </div>
+      <div className="space-y-4 mb-5">
+        <h3 className={` text-2xl font-medium mb-2 text-gray-700`}>
+          Different sizes
+        </h3>
+        <div className="flex flex-wrap gap-3">
+          <Button
+            text="Small"
+            color="primary"
+            size="small"
+            onClick={() => setClickCount(clickCount + 1)}
+          />
+          <Button
+            text="Medium (Default)"
+            color="primary"
+            size="medium"
+            onClick={() => setClickCount(clickCount + 1)}
+          />
+          <Button
+            text="Large"
+            color="primary"
+            size="large"
+            onClick={() => setClickCount(clickCount + 1)}
+          />
+        </div>
+      </div>
+      <div className="space-y-4 mb-5">
+        <h3 className={`text-2xl mb-2 font-medium text-gray-700`}>
+          Disabled State
+        </h3>
+        <div className="flex flex-wrap gap-3">
+          <Button
+            text="Enabled"
+            color="primary"
+            onClick={() => setClickCount(clickCount + 1)}
+          />
+          <Button
+            text="Disabled"
+            color="primary"
+            disabled={true}
+            onClick={() => alert("This button is disabled")}
+          />
+        </div>
+      </div>
+      <div className="space-y-4 rounded-lg bg-green-100 p-3">
+        <p>
+          Click Count:{" "}
+          <span className="text-blue-500 font-bold">{clickCount}</span>
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default BasicProps;
